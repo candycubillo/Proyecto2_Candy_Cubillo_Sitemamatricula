f96de73a16c955eecdb32778ccc9c0d4d4f3c14c
candycubillo <candycubillo1@gmail.com>
2023-04-22 10:16:23 -0600

    En la clase estudiante se coloca su constructor, con los metodos de la clase profeestudiante

--- ClaseEstudiante.py
+++ ClaseEstudiante.py
@@ -2,7 +2,7 @@ from ClaseProfesor import ProfeEstudiante # se importa claseProfesor y clase Pro
 # Clase Estudiante que guarda la información de los estudiantes de la institución
 class Estudiante(ProfeEstudiante): # SE CREA CLASE ESTUDIANTE QUE HEREDA DE LA CLASE metodos de la clase Profestudiante
 
-    #constructor de la clase
+    #constructor de la clase estudiante
     def _init_(self, cedula, nombre, fechaNacimiento, nCelular, grupo): #ATRIBUTOS DE LA CLASE
         self.cedula = cedula  # CEDULA DEL ESTUDIANTE
         self.nombre = nombre  # NOMBRE DEL ESTUDIANTE
