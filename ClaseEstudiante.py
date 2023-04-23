4d508eb6b035edfdab132365b851591f1b29a883
candycubillo <candycubillo1@gmail.com>
2023-04-22 18:55:47 -0600

    Se termina de crear las funciones y botones en el main

--- ClaseEstudiante.py
+++ ClaseEstudiante.py
@@ -1,7 +1,6 @@
 from ClaseProfesor import ProfeEstudiante # se importa claseProfesor y clase Profeestudiante
 # Clase Estudiante que guarda la información de los estudiantes de la institución
 class Estudiante(ProfeEstudiante): # SE CREA CLASE ESTUDIANTE QUE HEREDA DE LA CLASE metodos de la clase Profestudiante
-
     #constructor de la clase estudiante
     def _init_(self, cedula, nombre, fechaNacimiento, nCelular, grupo): #ATRIBUTOS DE LA CLASE
         self.cedula = cedula  # CEDULA DEL ESTUDIANTE
